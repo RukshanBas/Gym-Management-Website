6977d646cae4b39ee29eed2963136ff0dcfb4f79
RukshanBas <91020698+RukshanBas@users.noreply.github.com>
2024-12-15 12:20:39 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Gym-Management-Website
+
+# Gym Management System
+
+## Overview
+This Gym Management System is a web-based application designed to for users to access the services we offer and manage there gym account with us. They will be able to create an account and sign up for a memebership, classes, and a personal trainer.
+
+
+## Installation
+
+### Prerequisites
+- Node.js (v14 or later)
+- npm (Node Package Manager)
+
+### Steps
+1. Download and Unzip the folder from the GitHub repository
+
+2. Navigate to the project directory:
+   ```bash
+   cd gym-management
+   ```
+
+3. Install dependencies:
+   ```bash
+   npm install
+   ```
+
+4. Link the database in the `db.js` file
+
+5. Start the server:
+   ```bash
+   node server.js
+   ```
+
+5. Open your browser and navigate to `http://localhost:3000`.
+
+## Usage
+1. Register as a new user or log in with your credentials.
+2. Access various features such as member management, workout plans, and schedules.
+
+## Technologies Used
+- **Backend**: Node.js, Express.js
+- **Frontend**: HTML, CSS, JavaScript
+- **Database**: MySQL
+
+---
+Enjoy using the Gym Management System!
---
+++ server.js
@@ -0,0 +1,63 @@
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session"
+import passport from "./config/passport.js";
+//import routes from the route folder
+import homepage from "./routes/home_Router.js"
+import classpage from "./routes/class_Router.js"
+import loginpage from "./routes/login_Router.js"
+import logoutpage from "./routes/logout_Router.js"
+import signuppage from "./routes/signup_Router.js"
+import membershippage from "./routes/membership_Router.js";
+import contactUspage from "./routes/contactUs_Router.js";
+import profilepage from "./routes/profile_Router.js";
+import paymentpage from "./routes/payment_Router.js";
+import pt_page from "./routes/personalTraining_Router.js";
+
+
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');// Set EJS as the view engine
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static("public"))
+app.use(express.json());
+
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUnitialized: false,
+    cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+// Middleware to pass user data to every template
+app.use((req, res, next) => {
+    if (req.isAuthenticated() && req.user) {
+    
+    res.locals.user = req.session.passport ? req.session.passport.user : null;}
+    else {
+        // If not authenticated, set `res.locals.id` to an empty string
+        res.locals.user = '';
+    }
+    next();
+});
+
+
+app.use("/", homepage);
+app.use("/",classpage)
+app.use("/",loginpage)
+app.use("/",logoutpage)
+app.use("/",signuppage)
+app.use("/",membershippage);
+app.use("/",contactUspage);
+app.use("/",profilepage);
+app.use("/",paymentpage);
+app.use("/", pt_page);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
